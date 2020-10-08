b2aaf9fe7dd8f2f0f42d8354f77fd1f29641f97e
samrodriguez <melitor@gmail.com>
2020-10-08 10:46:21 -0600

    Error en el edit

--- src/Form/PacienteType.php
+++ src/Form/PacienteType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Paciente;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +14,28 @@ class PacienteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dui')
-            ->add('expediente')
-            ->add('nombre')
-            ->add('fecha')
-            ->add('direccion')
+            ->add('dui',TextType::class, [
+                'label' => 'DUI',
+                'attr' => [ 'class' => 'form-control', 'autofocus' => 'autofocus']
+            ])
+            ->add('expediente',TextType::class, [
+                'label' => 'No de Expediente *',
+                'attr' => [ 'class' => 'form-control']
+            ])
+            ->add('nombre',TextType::class, [
+                'label' => 'Nombre Completo *',
+                'attr' => [ 'class' => 'form-control']
+            ])
+            ->add('fecha',DateType::class, [
+                'widget' => 'choice',
+                'label' => 'Fecha de nacimiento *',
+            ])
+            ->add('direccion',TextType::class, [
+                'label' => 'Direccion',
+                'attr' => [ 'class' => 'form-control']
+            ])
             ->add('telefono')
+
         ;
     }
 
