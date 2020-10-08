4b29ba4875d1a4d953608db13c3ab79cb71205f3
samrodriguez <melitor@gmail.com>
2020-10-08 00:10:40 -0600

    Creando el menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,8 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import '../styles/app.css';
+import '../styles/global.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
